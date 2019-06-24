93e590b351891e4b7685b801d9e5e4353c31a6e2
lyl <liuyl5@asiainfo.com>
2019-06-24 09:57:36 +0800

    add redis

--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,6 @@ export MYSQL_DATA=$PWD/mysqldata
 mkdir -p mysqldata
 export UPLOAD_DATA=$PWD/uploaddata
 mkdir -p uploaddata
+export REDIS_DATA=$PWD/redisdata
+mkdir -p redisdata
 docker-compose up -d
