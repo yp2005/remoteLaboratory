e610c723625c7c0e57d6f41c3cf5820bed927a68
yupeng <18710095921@163.com>
2019-07-11 15:15:28 +0800

    token -> Authorization

--- src/main/java/com/remoteLaboratory/config/CorsFilter.java
+++ src/main/java/com/remoteLaboratory/config/CorsFilter.java
@@ -25,7 +25,7 @@ class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, token");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("X-Frame-Options", "SAMEORIGIN");
         if (!"OPTIONS".equals(request.getMethod())) {
